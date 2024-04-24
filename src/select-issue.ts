03ca9b79af2b624f1205b9379dc5a7e75f5fd8c8
Christoph Thiel <christoph.thiel@assecosol.com>
2024-04-24 15:25:48 +0200

    pick issue from assigned issues

--- src/select-issue.ts
+++ src/select-issue.ts
@@ -1,14 +1,21 @@
 import * as vscode from "vscode";
-import { fetchAssignedIssues } from "./service";
-export async function selectIssue() {
-  const issues = await fetchAssignedIssues();
-  const pick = await vscode.window.showQuickPick(issues, {
+import { jiraClientService } from "./extension";
+import { IJiraIssue } from "./jira";
+export async function selectIssue(): Promise<string | undefined> {
+  let issues: IJiraIssue[] = [];
+  let quickPicks: string[] = [];
+  const userEmailAddress = jiraClientService.getCurrentUser()?.emailAddress;
+  if (userEmailAddress) {
+    issues = await jiraClientService.getIssuesForUser(userEmailAddress);
+  }
+  if (issues.length !== 0) {
+    quickPicks = issues.map((issue) => {
+      return `${issue.key} - ${issue.fields.summary}`;
+    });
+  }
+  const pick = await vscode.window.showQuickPick(quickPicks, {
     placeHolder: "Select an issue to log time",
   });
 
-  if (pick) {
-    vscode.window.showInformationMessage(`Selected Issue: ${pick.label}`);
-    return pick.issueId;
-  }
-  return null;
+  return pick?.substring(0, pick.indexOf(" - "));
 }
